30bf1b9dc8c7dcd02f94d1803ab88e8afbab6188
talshbt <53658521+talshbt@users.noreply.github.com>
2021-05-03 11:05:31 +0300

    add button next and lottie click hand

--- client/src/app/intro/home-page/home-page.component.ts
+++ client/src/app/intro/home-page/home-page.component.ts
@@ -6,62 +6,119 @@ import { Slider } from './slider.model';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  mouseover = false;
 
-  sliders: Slider[] = [
-    {
-      options: {
-       path: '/assets/lotties/2523-loading-traveling.json',
-       loop: true
-      },
-      title: 'לטוס לחו"ל'
-    },
-    {
-      options: {
-       path: '/assets/lotties/25200-student.json',
-       loop: true
-      },
-      title: 'ללמוד'
-    },
-    {
-      options: {
-       path: '/assets/lotties/24185-buying-a-property-ed-najarro-painter2.json',
-       loop: true
-      },
-      title: 'לשפץ'
-    },
-    {
-      options: {
-       path: '/assets/lotties/help.json',
-       loop: true
-      },
-      title: 'לעזור'
-    },
-    {
-      options: {
-       path: '/assets/lotties/payment.json',
-       loop: true
-      },
-      title: 'לסגור חוב'
-    },
-    {
-      options: {
-       path: '/assets/lotties/39387-business-team.json',
-       loop: true
-      },
-      title: 'לפתוח עסק'
-    },
-    {
-      options: {
-       path: '/assets/lotties/fullfill-dream.json',
-       loop: true
-      },
-      title: 'חלום אחר'
-    },
 
-  ]
+  ngOnInit(){
+    //this.sliders = this.mouseoutSliders;
+  }
+
+
+  sliders: Slider[] = [ {
+    options: {
+     path: '/assets/lotties/2523-loading-traveling.json',
+     loop: true,
+    },
+    title: 'לטוס לחו"ל',
+  },
+  {
+    options: {
+     path: '/assets/lotties/25200-student.json',
+     loop: true,
+    },
+    title: 'ללמוד',
+  },
+  {
+    options: {
+     path: '/assets/lotties/24185-buying-a-property-ed-najarro-painter2.json',
+     loop: true
+    },
+    title: 'לשפץ',
+  },
+  {
+    options: {
+     path: '/assets/lotties/help.json',
+     loop: true
+    },
+    title: 'לעזור',
+
+  },
+  {
+    options: {
+     path: '/assets/lotties/payment.json',
+     loop: true
+    },
+    title: 'לסגור חוב',
+
+  },
+  {
+    options: {
+     path: '/assets/lotties/39387-business-team.json',
+     loop: true,
+    },
+    title: 'לפתוח עסק',
+
+  },
+  {
+    options: {
+     path: '/assets/lotties/fullfill-dream.json',
+     loop: true,
+    },
+    title: 'חלום אחר',
+
+
+  },]
+
+
+  clickSlider:Slider =  {
+    options: {
+     path: '/assets/lotties/touch.json',
+     loop: true,
+    },
+    title: 'המשך',
+  }
+
+  nextSlider:Slider =  {
+    options: {
+     path: '/assets/lotties/26866-next-button.json',
+     loop: true,
+    },
+    title: 'המשך',
+  }
+  nextSliders: Slider[] = [  {
+    options: {
+     path: '/assets/lotties/26866-next-button.json',
+     loop: true,
+    },
+    title: 'המשך',
+  },
+
+  {
+    options: {
+     path: '/assets/lotties/touch.json',
+     loop: true,
+    },
+    title: 'המשך',
+  }
+
+]
+
+
+
 
   animationCreated(animationItem: AnimationItem): void {
     console.log(animationItem);
+  }
+
+  onMouseover(){
+    // alert('Mouseover')
+   //  this.sliders = this.mouseoutSliders;
+
+  }
+
+  onMouseout(){
+    // alert('Mouseout')
+    // this.sliders = this.mouseoverSliders;
   }
 }
