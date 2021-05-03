7e70569aab5ac00c8ac1bfce043b3eddf76d9067
talshbt <53658521+talshbt@users.noreply.github.com>
2021-05-03 22:01:48 +0300

    add event emmiter

--- client/src/app/intro/home-page/home-page.component.ts
+++ client/src/app/intro/home-page/home-page.component.ts
@@ -1,128 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-import { AnimationItem } from 'lottie-web';
-import { Slider } from './slider.model';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { AnimationItem } from "lottie-web";
+import { Slider } from "./slider.model";
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.scss"],
 })
-export class HomePageComponent implements OnInit{
+export class HomePageComponent{
+  @Output() next = new EventEmitter<void>();
+
   mouseover = false;
-
-
-  ngOnInit(){
-    //this.sliders = this.mouseoutSliders;
-  }
-
-
-  sliders: Slider[] = [ {
-    options: {
-     path: '/assets/lotties/2523-loading-traveling.json',
-     loop: true,
+  sliders: Slider[] = [
+    {
+      options: {
+        path: "/assets/lotties/2523-loading-traveling.json",
+        loop: true,
+      },
+      title: 'לטוס לחו"ל',
     },
-    title: 'לטוס לחו"ל',
-  },
-  {
-    options: {
-     path: '/assets/lotties/25200-student.json',
-     loop: true,
+    {
+      options: {
+        path: "/assets/lotties/25200-student.json",
+        loop: true,
+      },
+      title: "ללמוד",
     },
-    title: 'ללמוד',
-  },
-  {
-    options: {
-     path: '/assets/lotties/24185-buying-a-property-ed-najarro-painter2.json',
-     loop: true
+    {
+      options: {
+        path:
+          "/assets/lotties/24185-buying-a-property-ed-najarro-painter2.json",
+        loop: true,
+      },
+      title: "לשפץ",
     },
-    title: 'לשפץ',
-  },
-  {
-    options: {
-     path: '/assets/lotties/help.json',
-     loop: true
+    {
+      options: {
+        path: "/assets/lotties/help.json",
+        loop: true,
+      },
+      title: "לעזור",
     },
-    title: 'לעזור',
-
-  },
-  {
-    options: {
-     path: '/assets/lotties/payment.json',
-     loop: true
+    {
+      options: {
+        path: "/assets/lotties/payment.json",
+        loop: true,
+      },
+      title: "לסגור חוב",
     },
-    title: 'לסגור חוב',
-
-  },
-  {
-    options: {
-     path: '/assets/lotties/39387-business-team.json',
-     loop: true,
+    {
+      options: {
+        path: "/assets/lotties/39387-business-team.json",
+        loop: true,
+      },
+      title: "לפתוח עסק",
     },
-    title: 'לפתוח עסק',
-
-  },
-  {
-    options: {
-     path: '/assets/lotties/fullfill-dream.json',
-     loop: true,
+    {
+      options: {
+        path: "/assets/lotties/fullfill-dream.json",
+        loop: true,
+      },
+      title: "חלום אחר",
     },
-    title: 'חלום אחר',
+  ];
 
-
-  },]
-
-
-
-
-  nextSlider:Slider =  {
+  dotsSlider: Slider = {
     options: {
-     path: '/assets/lotties/26866-next-button.json',
-     loop: true,
+      path: "/assets/lotties/dots.json",
+      loop: true,
     },
-    title: 'המשך',
-  }
+    title: "המשך",
+  };
 
-
-  clickSlider:Slider =  {
+  clickSlider: Slider = {
     options: {
-     path: '/assets/lotties/10742-hand-tap.json',
-     loop: true,
+      path: "/assets/lotties/2910-hand-tap.json",
+      loop: true,
     },
-    title: 'המשך',
-  }
-
-  nextSliders: Slider[] = [  {
-    options: {
-     path: '/assets/lotties/26866-next-button.json',
-     loop: true,
-    },
-    title: 'המשך',
-  },
-
-  {
-    options: {
-     path: '/assets/lotties/53969-click.json',
-     loop: true,
-    },
-    title: 'המשך',
-  }
-
-]
-
-
-
+    title: "המשך",
+  };
 
   animationCreated(animationItem: AnimationItem): void {
     console.log(animationItem);
-  }
-
-  onMouseover(){
-    // alert('Mouseover')
-   //  this.sliders = this.mouseoutSliders;
-
-  }
-
-  onMouseout(){
-    // alert('Mouseout')
-    // this.sliders = this.mouseoverSliders;
   }
 }
